f71de6720e092f83d5f3972d3ca31f39cbb55e7e
金正朋也 <mhtnim1023@gmail.com>
2017-09-02 10:34:52 +0900

    add highlight

--- app/assets/javascripts/sortable_table.js
+++ app/assets/javascripts/sortable_table.js
@@ -3,6 +3,7 @@ $(function(){
   $('.table-sortable').disableSelection();
   $('.table-sortable').bind('sortstop', function(e, ui){
     item = ui.item;
+    item.children('td').effect('highlight');
     name = item.attr('data-name');
     order = item.index();
     id = item.attr('data-id');
